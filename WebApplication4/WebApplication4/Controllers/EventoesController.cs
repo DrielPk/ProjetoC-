e59df5cf214bc76b0be8e0d00aff42b213b18662
adriel <adriel.belluomini@gmail.com>
2017-11-20 23:47:50 -0200

    Projeto Simples para a Evolua

---
+++ WebApplication4/WebApplication4/Controllers/EventoesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class EventoesController : Controller
+    {
+        private AgendaEntities db = new AgendaEntities();
+
+        // GET: Eventoes
+        public ActionResult Index(string contato_evt, string nome_evt)
+        {
+            ViewBag.contato_evt = (from c in db.Eventoes select c.contato_evt).Distinct();
+            ViewBag.nome_evt = (from c in db.Eventoes select c.nome_evt).Distinct();
+            var model = from c in db.Eventoes
+                        orderby c.contato_evt
+                        where c.contato_evt == contato_evt || contato_evt.Equals(null) || contato_evt.Equals("")
+                        where c.nome_evt == nome_evt || nome_evt.Equals(null) || nome_evt.Equals("")
+                        select c;
+
+
+            return View(model);
+        }
+
+        // GET: Eventoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Evento evento = db.Eventoes.Find(id);
+            if (evento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(evento);
+        }
+
+        // GET: Eventoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Eventoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_evento,nome_evt,data_evt,contato_evt")] Evento evento)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Eventoes.Add(evento);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(evento);
+        }
+
+        // GET: Eventoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Evento evento = db.Eventoes.Find(id);
+            if (evento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(evento);
+        }
+
+        // POST: Eventoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_evento,nome_evt,data_evt,contato_evt")] Evento evento)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(evento).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(evento);
+        }
+
+        // GET: Eventoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Evento evento = db.Eventoes.Find(id);
+            if (evento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(evento);
+        }
+
+        // POST: Eventoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Evento evento = db.Eventoes.Find(id);
+            db.Eventoes.Remove(evento);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
